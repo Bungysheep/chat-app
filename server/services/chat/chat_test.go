00197ce197596b3dbdc4425009794528a5b07173
James Embongbulan <james.embongbulan@gmail.com>
2019-09-16 10:18:04 +0800

    Revised Chat service subscription behavior

--- server/services/chat/chat_test.go
+++ server/services/chat/chat_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/bungysheep/chat-app/server/api/chat"
 	"github.com/bungysheep/chat-app/server/api/chat/mock"
@@ -42,22 +43,6 @@ func TestSendEmptyMessage(t *testing.T) {
 	}
 }
 
-func TestSendAMessage(t *testing.T) {
-	ctx := context.Background()
-
-	chatSvc := NewChatServiceServer()
-
-	sendReq := &chat.SendRequest{Message: &chat.Message{Text: "Hello World!"}}
-	sendResp, err := chatSvc.Send(ctx, sendReq)
-	if err != nil {
-		t.Errorf("Expected no Error, but got %v", err)
-	}
-
-	if sendResp == nil {
-		t.Errorf("Expected a Send Response, but got no Send Response")
-	}
-}
-
 func TestSendAndSubscribeMessage(t *testing.T) {
 	ctl := gomock.NewController(t)
 	defer ctl.Finish()
@@ -68,19 +53,24 @@ func TestSendAndSubscribeMessage(t *testing.T) {
 
 	mChatSubSvc := mock.NewMockChatService_SubscribeServer(ctl)
 
+	done := make(chan bool, 1)
 	go func() {
-		mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{Text: "Hello World!"}}).Return(nil)
-		mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{Text: "This is gRpc"}}).Return(nil)
-
-		chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "Hello World!"}})
-		chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "This is gRpc"}})
-
-		chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "<exit>"}})
+		if err := chatSvc.Subscribe(&chat.SubscribeRequest{}, mChatSubSvc); err != nil {
+			t.Errorf("Expected no Error, but got %v", err)
+		}
+		done <- true
 	}()
 
-	if err := chatSvc.Subscribe(&chat.SubscribeRequest{}, mChatSubSvc); err != nil {
-		t.Errorf("Expected no Error, but got %v", err)
-	}
+	mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{Text: "Hello World!"}}).Return(nil)
+	mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{Text: "This is gRpc"}}).Return(nil)
+	mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{Text: "<exit>"}}).Return(nil)
+
+	time.Sleep(30 * time.Millisecond)
+	chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "Hello World!"}})
+	chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "This is gRpc"}})
+	chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "<exit>"}})
+
+	<-done
 }
 
 func TestSendAndSubscribeMessageWhenStreamResponseFail(t *testing.T) {
@@ -93,15 +83,20 @@ func TestSendAndSubscribeMessageWhenStreamResponseFail(t *testing.T) {
 
 	mChatSubSvc := mock.NewMockChatService_SubscribeServer(ctl)
 
+	done := make(chan bool, 1)
 	go func() {
-		mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{Text: "Hello World!"}}).Return(nil)
-		mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{Text: "This is gRpc"}}).Return(fmt.Errorf("Unable to stream message 'This is gRpc'"))
-
-		chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "Hello World!"}})
-		chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "This is gRpc"}})
+		if err := chatSvc.Subscribe(&chat.SubscribeRequest{}, mChatSubSvc); err == nil {
+			t.Errorf("Expected a Error, but got no Error")
+		}
+		done <- true
 	}()
 
-	if err := chatSvc.Subscribe(&chat.SubscribeRequest{}, mChatSubSvc); err == nil {
-		t.Errorf("Expected a Error, but got no Error")
-	}
+	mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{Text: "Hello World!"}}).Return(nil)
+	mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{Text: "This is gRpc"}}).Return(fmt.Errorf("Unable to stream message 'This is gRpc'"))
+
+	time.Sleep(30 * time.Millisecond)
+	chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "Hello World!"}})
+	chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "This is gRpc"}})
+
+	<-done
 }
