2488738f40ee2a726f4e93f3dc4d93becc0fda64
James Embongbulan <james.embongbulan@gmail.com>
2019-09-19 21:44:26 +0800

    Ensured 2 clients can send and subscribe messages

--- server/services/chat/chat_test.go
+++ server/services/chat/chat_test.go
@@ -9,8 +9,6 @@ import (
 	"github.com/bungysheep/chat-app/server/api/chat"
 	"github.com/bungysheep/chat-app/server/api/chat/mock"
 	"github.com/golang/mock/gomock"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func TestSendNilMessage(t *testing.T) {
@@ -45,29 +43,6 @@ func TestSendEmptyMessage(t *testing.T) {
 	}
 }
 
-func TestSendMessageWithoutSubscriber(t *testing.T) {
-	ctl := gomock.NewController(t)
-	defer ctl.Finish()
-
-	ctx := context.Background()
-
-	chatSvc := NewChatServiceServer()
-
-	resp, err := chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "Hello World!"}})
-	if err == nil {
-		t.Errorf("Expected an Error, but got no Error")
-	} else {
-		s, _ := status.FromError(err)
-		if s.Code() != codes.NotFound {
-			t.Errorf("Expected '%s' error, but got '%s'", codes.NotFound, s.Code())
-		}
-	}
-
-	if resp != nil {
-		t.Errorf("Expected no SendResponse, but got %v", resp)
-	}
-}
-
 func TestSendAndSubscribeMessage(t *testing.T) {
 	ctl := gomock.NewController(t)
 	defer ctl.Finish()
@@ -78,22 +53,21 @@ func TestSendAndSubscribeMessage(t *testing.T) {
 
 	mChatSubSvc := mock.NewMockChatService_SubscribeServer(ctl)
 
-	done := make(chan bool, 1)
+	done := make(chan bool)
 	go func() {
-		if err := chatSvc.Subscribe(&chat.SubscribeRequest{}, mChatSubSvc); err != nil {
+		mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{SendId: "001", Text: "Hello World!"}}).Return(nil)
+		mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{SendId: "001", Text: "This is gRpc"}}).Return(nil)
+
+		if err := chatSvc.Subscribe(&chat.SubscribeRequest{SubId: "001"}, mChatSubSvc); err != nil {
 			t.Errorf("Expected no Error, but got %v", err)
 		}
 		done <- true
 	}()
 
-	mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{Text: "Hello World!"}}).Return(nil)
-	mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{Text: "This is gRpc"}}).Return(nil)
-	mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{Text: "<exit>"}}).Return(nil)
-
-	time.Sleep(30 * time.Millisecond)
-	chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "Hello World!"}})
-	chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "This is gRpc"}})
-	chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "<exit>"}})
+	time.Sleep(1 * time.Second)
+	chatSvc.Send(ctx, &chat.SendRequest{SubId: "001", Message: &chat.Message{SendId: "001", Text: "Hello World!"}})
+	chatSvc.Send(ctx, &chat.SendRequest{SubId: "001", Message: &chat.Message{SendId: "001", Text: "This is gRpc"}})
+	chatSvc.Send(ctx, &chat.SendRequest{SubId: "001", Message: &chat.Message{Text: "<exit>"}})
 
 	<-done
 }
@@ -108,20 +82,111 @@ func TestSendAndSubscribeMessageWhenStreamResponseFail(t *testing.T) {
 
 	mChatSubSvc := mock.NewMockChatService_SubscribeServer(ctl)
 
-	done := make(chan bool, 1)
+	done := make(chan bool)
 	go func() {
-		if err := chatSvc.Subscribe(&chat.SubscribeRequest{}, mChatSubSvc); err == nil {
+		mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{SendId: "001", Text: "Hello World!"}}).Return(nil)
+		mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{SendId: "001", Text: "This is gRpc"}}).Return(fmt.Errorf("Unable to stream message 'This is gRpc'"))
+
+		if err := chatSvc.Subscribe(&chat.SubscribeRequest{SubId: "001"}, mChatSubSvc); err == nil {
 			t.Errorf("Expected an Error, but got no Error")
 		}
 		done <- true
 	}()
 
-	mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{Text: "Hello World!"}}).Return(nil)
-	mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{Text: "This is gRpc"}}).Return(fmt.Errorf("Unable to stream message 'This is gRpc'"))
+	time.Sleep(1 * time.Second)
+	chatSvc.Send(ctx, &chat.SendRequest{SubId: "001", Message: &chat.Message{SendId: "001", Text: "Hello World!"}})
+	chatSvc.Send(ctx, &chat.SendRequest{SubId: "001", Message: &chat.Message{SendId: "001", Text: "This is gRpc"}})
 
+	<-done
+}
+
+func TestSendMessageToOtherSubscriber(t *testing.T) {
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	ctx := context.Background()
+
+	chatSvc := NewChatServiceServer()
+
+	done := make(chan bool, 2)
+	go func() {
+		mChatSubSvc := mock.NewMockChatService_SubscribeServer(ctl)
+
+		mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{SendId: "002", Text: "This is gRpc"}}).Return(nil)
+
+		if err := chatSvc.Subscribe(&chat.SubscribeRequest{SubId: "001"}, mChatSubSvc); err != nil {
+			t.Errorf("Expected no Error, but got %v", err)
+		}
+		done <- true
+	}()
+
+	go func() {
+		mChatSubSvc := mock.NewMockChatService_SubscribeServer(ctl)
+
+		mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{SendId: "001", Text: "Hello World!"}}).Return(nil)
+
+		if err := chatSvc.Subscribe(&chat.SubscribeRequest{SubId: "002"}, mChatSubSvc); err != nil {
+			t.Errorf("Expected no Error, but got %v", err)
+		}
+		done <- true
+	}()
+
+	time.Sleep(1 * time.Second)
+
+	chatSvc.Send(ctx, &chat.SendRequest{SubId: "002", Message: &chat.Message{SendId: "001", Text: "Hello World!"}})
 	time.Sleep(30 * time.Millisecond)
-	chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "Hello World!"}})
-	chatSvc.Send(ctx, &chat.SendRequest{Message: &chat.Message{Text: "This is gRpc"}})
+	chatSvc.Send(ctx, &chat.SendRequest{SubId: "001", Message: &chat.Message{SendId: "002", Text: "This is gRpc"}})
 
+	chatSvc.Send(ctx, &chat.SendRequest{SubId: "002", Message: &chat.Message{Text: "<exit>"}})
+	chatSvc.Send(ctx, &chat.SendRequest{SubId: "001", Message: &chat.Message{Text: "<exit>"}})
+
+	<-done
+	<-done
+}
+
+func TestSendMessageToOtherSubscriberWhenStreamResponseFail(t *testing.T) {
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	ctx := context.Background()
+
+	chatSvc := NewChatServiceServer()
+
+	done := make(chan bool, 2)
+	go func() {
+		mChatSubSvc := mock.NewMockChatService_SubscribeServer(ctl)
+
+		mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{SendId: "002", Text: "Hello World! 002"}}).Return(nil)
+		mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{SendId: "002", Text: "This is gRpc 002"}}).Return(fmt.Errorf("Unable to stream message 'This is gRpc'"))
+
+		if err := chatSvc.Subscribe(&chat.SubscribeRequest{SubId: "001"}, mChatSubSvc); err == nil {
+			t.Errorf("Expected no Error, but got %v", err)
+		}
+		done <- true
+	}()
+
+	go func() {
+		mChatSubSvc := mock.NewMockChatService_SubscribeServer(ctl)
+
+		mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{SendId: "001", Text: "Hello World! 001"}}).Return(nil)
+		mChatSubSvc.EXPECT().Send(&chat.SubscribeResponse{Message: &chat.Message{SendId: "001", Text: "This is gRpc 001"}}).Return(fmt.Errorf("Unable to stream message 'This is gRpc'"))
+
+		if err := chatSvc.Subscribe(&chat.SubscribeRequest{SubId: "002"}, mChatSubSvc); err == nil {
+			t.Errorf("Expected no Error, but got %v", err)
+		}
+		done <- true
+	}()
+
+	time.Sleep(1 * time.Second)
+
+	chatSvc.Send(ctx, &chat.SendRequest{SubId: "002", Message: &chat.Message{SendId: "001", Text: "Hello World! 001"}})
+	time.Sleep(30 * time.Millisecond)
+	chatSvc.Send(ctx, &chat.SendRequest{SubId: "001", Message: &chat.Message{SendId: "002", Text: "Hello World! 002"}})
+	time.Sleep(30 * time.Millisecond)
+	chatSvc.Send(ctx, &chat.SendRequest{SubId: "002", Message: &chat.Message{SendId: "001", Text: "This is gRpc 001"}})
+	time.Sleep(30 * time.Millisecond)
+	chatSvc.Send(ctx, &chat.SendRequest{SubId: "001", Message: &chat.Message{SendId: "002", Text: "This is gRpc 002"}})
+
+	<-done
 	<-done
 }
